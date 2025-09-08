18697599d5ca07cd5b549db1048720ffa8f0f0a9
esmahoney <erinnmahoney@yahoo.com>
2025-09-08 19:34:59 +0000

    update groups notes with more examples

--- cyberchase/groups.sql
+++ cyberchase/groups.sql
@@ -1,7 +1,12 @@
 -- Demonstrates aggregation by groups with GROUP BY
 -- Uses longlist.db
 
--- Finds average rating for each book
+-- Returns book_id and average rating without rounding
+SELECT "book_id", AVG("rating") AS "average rating" 
+   ...> FROM "ratings"
+   ...> GROUP BY "book_id";
+
+-- Finds average rounded rating for each book
 SELECT "book_id", ROUND(AVG("rating"), 2) AS "average rating" FROM "ratings"
 GROUP BY "book_id";
 
@@ -15,3 +20,8 @@ SELECT "title", ROUND(AVG("rating"), 2) AS "average rating" FROM "ratings"
 JOIN "books" ON "books"."id" = "ratings"."book_id"
 GROUP BY "book_id"
 HAVING "average rating" > 4.0;
+ORDER BY "average rating" DESC;
+
+-- Count of ratings per book
+SELECT "book_id", COUNT("rating") FROM "ratings"
+GROUP BY "book_id"
